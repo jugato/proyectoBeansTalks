a7aa2e8312e348a701a3935d3e34de4ff16b9d3f
Youlvis Jose Jimenez Peñate <yojimene@bancolombia.com.co>
2022-08-04 16:50:23 -0500

    version 3

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 const morgan = require('morgan');
 const app = express();
 
 //importing routes
 const animeRoute = require('./routes/animeRoute');
-const exp = require('constants');
 
 //setting
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-//app.use('/public', express.static('public'))
+
 
 //middlewares
 app.use(morgan('dev'));
@@ -32,7 +32,8 @@ app.use('/', animeRoute);
 //res.sendFile(path.resolve(__dirname,'index.html'));
 
 // static files
-//app.get('style.css');
+//app.get('/public/css/styles.css', css );
+app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), () => {
     console.log(`server on port ${app.get('port')}`);
